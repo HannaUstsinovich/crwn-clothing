6d7c6ac7e9dc2403d60eea9256add7394afcfc10
HannaUstsinovich <hannaustinovich@gmail.com>
2021-08-09 18:18:39 +0300

    finished sign in handleSubmit method

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -22,8 +22,6 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
   if (!snapShot.exists) {
     const { displayName, email } = userAuth;
     const createdAt = new Date();
-    console.log(displayName);
-    console.log(displayName);
 
     try {
       await userRef.set({

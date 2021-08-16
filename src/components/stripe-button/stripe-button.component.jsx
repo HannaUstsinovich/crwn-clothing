498481b7722da6e5ed37c6c5d97e6d9bbb568e42
HannaUstsinovich <hannaustinovich@gmail.com>
2021-08-16 23:29:57 +0300

    added stripe integration, updated favicon

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    "pk_test_51JPC3UHY05o3XMbinGXNLPfnoM7Wc5qNCpx6q6jKM9jgBinq15N2qHmkzAiiBPhYOEbNoPb1ksOEOjrvfoO8FlV900cGX4eT8d";
+
+  const onToken = (token) => {
+    console.log(token);
+    alert("Payment Successful");
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="CRWN Clothing Ltd."
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
